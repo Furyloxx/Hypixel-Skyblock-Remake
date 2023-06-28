0e2b7f0173c74f6281be73da613fae4a28de81e4
dumbo-the-developer <gak1901p@gmail.com>
2023-06-28 19:50:04 +0530

    .

--- src/main/java/me/adarsh/godspunkycore/listener/ServerPingListener.java
+++ src/main/java/me/adarsh/godspunkycore/listener/ServerPingListener.java
@@ -4,10 +4,12 @@ import me.adarsh.godspunkycore.util.SUtil;
 import org.bukkit.ChatColor;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.server.ServerListPingEvent;
+import org.bukkit.util.CachedServerIcon;
 
 public class ServerPingListener extends PListener {
     @EventHandler
     public void onServerPing(ServerListPingEvent e) {
         e.setMotd(ChatColor.GREEN + " GodSPUNKY SKYBLOCK" + ChatColor.RED + "[1.8-1.16]\n                     " + SUtil.getRandomVisibleColor() + ChatColor.BOLD + "SKYBLOCK STUFF");
+        e.setMaxPlayers(200);
     }
 }
