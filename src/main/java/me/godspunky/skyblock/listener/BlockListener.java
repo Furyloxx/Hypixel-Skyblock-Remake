3e7f370bf24aa5a875b68e0f3385d06f9c9e6b9c
Hamza <kinghamza09088@gmail.com>
2023-08-25 05:39:25 -0700

    fixed launchPads

--- src/main/java/me/godspunky/skyblock/listener/BlockListener.java
+++ src/main/java/me/godspunky/skyblock/listener/BlockListener.java
@@ -83,6 +83,7 @@ public class BlockListener extends PListener {
                 LaunchPadHandler handler = new LaunchPadHandler();
                 handler.savePad(generator.getStart() , generator.getEnd() , generator.getStartLocation() , generator.getEndLocation() , generator.getTeleportLocation());
                 player.sendMessage(ChatColor.GREEN + "Created LaunchPad!");
+                SetLaunchPad.PAD_GENERATION_MAP.remove(player);
 
             }
 
