8ce5964d7706442c9f300f81471fbc320baaf616
Furyloxx <161728619+Furyloxx@users.noreply.github.com>
2024-09-03 13:37:35 +0530

    Update Skyblock.java

--- src/main/java/me/godspunky/skyblock/Skyblock.java
+++ src/main/java/me/godspunky/skyblock/Skyblock.java
@@ -482,7 +482,7 @@ public final class Skyblock extends JavaPlugin {
     }
 
     public String getPrefix() {
-        return ChatColor.translateAlternateColorCodes('&', "&7[&aSkypixel&3Skyblock&bCore&7] &f");
+        return ChatColor.translateAlternateColorCodes('&', "&7[&aHypixel&3Skyblock&bCore&7] &f");
     }
 
     public void sendMessage(String message) {
