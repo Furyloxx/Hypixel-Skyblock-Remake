d4960c11dd514f73ce140fac6e7cade3c3a286e4
Furyloxx <161728619+Furyloxx@users.noreply.github.com>
2024-09-11 07:48:46 +0530

    Update ServerPingListener.java

--- src/main/java/me/godspunky/skyblock/listener/ServerPingListener.java
+++ src/main/java/me/godspunky/skyblock/listener/ServerPingListener.java
@@ -8,7 +8,7 @@ import org.bukkit.event.server.ServerListPingEvent;
 public class ServerPingListener extends PListener {
     @EventHandler
     public void onServerPing(ServerListPingEvent e) {
-        e.setMotd(ChatColor.GREEN + "              Furyloxx Skyblock " + ChatColor.RED + "[1.8-1.21]\n                     " + SUtil.getRandomVisibleColor() + ChatColor.BOLD + "SKYBLOCK OP!!!");
+        e.setMotd(ChatColor.GREEN + "              StreamStorm Network " + ChatColor.RED + "[1.8-1.21]\n                     " + SUtil.getRandomVisibleColor() + ChatColor.BOLD + "SKYBLOCK OP!!!");
         e.setMaxPlayers(1000);
     }
 }
