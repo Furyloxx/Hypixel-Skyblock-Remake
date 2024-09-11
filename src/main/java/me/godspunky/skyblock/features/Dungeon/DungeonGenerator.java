0750228e89a8f027fbaec8b82e29f4a2c835cc1a
Furyloxx <161728619+Furyloxx@users.noreply.github.com>
2024-09-11 20:44:46 +0530

    Fixed Dungeon Generator

--- src/main/java/me/godspunky/skyblock/features/Dungeon/DungeonGenerator.java
+++ src/main/java/me/godspunky/skyblock/features/Dungeon/DungeonGenerator.java
@@ -64,7 +64,7 @@ public class DungeonGenerator {
                     });
         } catch (IOException e) {
             SLog.severe("Failed to copy world 'f1' to '" + worldname + "'!");
-            player.sendMessage(ChatColor.AQUA + "[FuryloxxTheDev] : " + ChatColor.RED + "Failed to create dungeon! " + ChatColor.GREEN + " Report to admin");
+            player.sendMessage(ChatColor.RED + "Failed to create dungeon! " + ChatColor.GREEN + " Report to admin");
             e.printStackTrace();
             return;
         }
@@ -73,7 +73,7 @@ public class DungeonGenerator {
 
         if (world == null) {
             SLog.severe("Failed to create the world '" + worldname + "'!");
-            player.sendMessage(ChatColor.AQUA + "[FuryloxxTheDev] : " + ChatColor.RED + "Failed to create dungeon! " + ChatColor.GREEN + " Report to admin");
+            player.sendMessage(ChatColor.RED + "Failed to create dungeon! " + ChatColor.GREEN + " Report to admin");
             return;
         }
 
@@ -85,7 +85,7 @@ public class DungeonGenerator {
                 warpParty(party);
             } catch (Exception e) {
                 SLog.severe("Error occurred while executing r method: " + e.getMessage());
-                player.sendMessage(ChatColor.AQUA + "[FuryloxxTheDev] : " + ChatColor.RED + "Failed to create dungeon! " + ChatColor.GREEN + " Report to admin");
+                player.sendMessage(ChatColor.RED + "Failed to create dungeon! " + ChatColor.GREEN + " Report to admin");
             }
         }, 1L);
 
