0fcdaca447d9c9c3ae626542a7ffe08b03b581a8
Furyloxx <161728619+Furyloxx@users.noreply.github.com>
2024-09-13 19:59:48 +0530

    Added Anubis (Hypixel Admin Item)

---
+++ src/main/java/me/godspunky/skyblock/features/item/armor/admin/Anubis.java
@@ -0,0 +1,35 @@
+package me.godspunky.skyblock.features.item.armor.admin;
+
+import me.godspunky.skyblock.features.item.*;
+
+public class Anubis implements MaterialFunction, SkullStatistics, ToolStatistics {
+    @Override
+    public String getDisplayName() {
+        return "Anubis";
+    }
+
+    @Override
+    public Rarity getRarity() {
+        return Rarity.LEGENDARY;
+    }
+
+    @Override
+    public GenericItemType getType() {
+        return GenericItemType.ARMOR;
+    }
+
+    @Override
+    public SpecificItemType getSpecificType() {
+        return SpecificItemType.HELMET;
+    }
+
+    @Override
+    public double getBaseHealth() {
+        return 3001;
+    }
+
+    @Override
+    public String getURL() {
+        return "4ca69fcee8ae9a9384533eb95f64474be505910d959ff4ac12611b3017e079d0";
+    }
+}
