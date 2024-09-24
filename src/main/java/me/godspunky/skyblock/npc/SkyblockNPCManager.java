239f0a350bb96b49752584270bfed8cfeeda3705
grish <grishmahat@gmail.com>
2024-09-24 22:11:47 +0545

    fix npc

--- src/main/java/me/godspunky/skyblock/npc/SkyblockNPCManager.java
+++ src/main/java/me/godspunky/skyblock/npc/SkyblockNPCManager.java
@@ -1,17 +1,19 @@
 package me.godspunky.skyblock.npc;
 
-import java.util.HashSet;
+import java.util.Collections;
 import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class SkyblockNPCManager {
-    private static final Set<SkyblockNPC> SKYBLOCK_NPCS = new HashSet<>();
+    private static final Set<SkyblockNPC> SKYBLOCK_NPCS = ConcurrentHashMap.newKeySet();
 
     public static void registerNPC(SkyblockNPC skyblockNPC){
-        SKYBLOCK_NPCS.add(skyblockNPC);
+        if (skyblockNPC != null) {
+            SKYBLOCK_NPCS.add(skyblockNPC);
+        }
     }
+
     public static Set<SkyblockNPC> getNPCS(){
-        return SKYBLOCK_NPCS;
+        return Collections.unmodifiableSet(SKYBLOCK_NPCS);
     }
-
-
 }
--- src/main/java/me/godspunky/skyblock/npc/hub/Banker.java
+++ src/main/java/me/godspunky/skyblock/npc/hub/Banker.java
@@ -9,59 +9,59 @@ public class Banker extends SkyblockNPC {
         super(new NPCParameters() {
 
             @Override
-            public String name() {
+            public String getName() {
                 return "Banker";
             }
 
             @Override
-            public String[] holograms() {
+            public String[] getHolograms() {
                 return new String[]{
                         "&fBanker" ,
                         "&e&lCLICK"
                 };
             }
             @Override
-            public String texture() {
+            public String getTexture() {
                 return "ewogICJ0aW1lc3RhbXAiIDogMTY5Mjk3NzczOTE1MywKICAicHJvZmlsZUlkIiA6ICI2Njg5MDJmYjI1YTY0NDBhODBmM2Y2MjZhYTk0MzBmYyIsCiAgInByb2ZpbGVOYW1lIiA6ICJCYW5rZXIiLAogICJzaWduYXR1cmVSZXF1aXJlZCIgOiB0cnVlLAogICJ0ZXh0dXJlcyIgOiB7CiAgICAiU0tJTiIgOiB7CiAgICAgICJ1cmwiIDogImh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvMmZkODNhY2NhOWFmM2JiYWQ3MDVmNzE0MzU1ZDk0MTA3NDEyY2E0ZWJiZDRjZTkzOTE2MGMxYmUxMGNjZDFhMiIsCiAgICAgICJtZXRhZGF0YSIgOiB7CiAgICAgICAgIm1vZGVsIiA6ICJzbGltIgogICAgICB9CiAgICB9LAogICAgIkNBUEUiIDogewogICAgICAidXJsIiA6ICJodHRwOi8vdGV4dHVyZXMubWluZWNyYWZ0Lm5ldC90ZXh0dXJlL2U3ZGZlYTE2ZGM4M2M5N2RmMDFhMTJmYWJiZDEyMTYzNTljMGNkMGVhNDJmOTk5OWI2ZTk3YzU4NDk2M2U5ODAiCiAgICB9CiAgfQp9";
             }
 
             @Override
-            public String signature() {
+            public String getSignature() {
                 return "mvjZz6+3WH5AuE2YKGJnyFbtfYMUFG2d2bimfJiUPWT8PrfeXurYbLLclpiuk16H/4th5+zMfdnPWRKIyIbWHa0UvSOjnRFTI1xSf5j1UbQKaG8lbwIZUcC6zPU/L/l8ei9X23aUFR+hvgG6OE2e2j4I0MKZ+bcaEcEVnVRQWdGG3XL6FxBcmlXrOtQhnHD6QX+rWgCh8XQYau0nDTXxVbRVCwf3EwjfdEKCic1Za0Bn6tZYv2hz8NvXwrfOQIjXMqFL7a+PPk8sYtdCqUt0oB7N5H+soJaFFPMKegCxF/7Ohk4YaXabVJwYlxxZDjf+ttfolRT8r+YZDlTQegykIKrlw1X9OSJq21vMFthXl+CDpkz3Mj0ClbORRWjzctBJ2Gp5PX5jaE2Fkwjt2kdpf/iurly+LcF5DQ7XD4mSTatUjm9M3nPuj1xjwZ7mmTIyCtwWV0ytqKjkLztEfU5ugx6DwpF1LjIKXW0LGZUHuIX/dYw4OMaNJ3BtQf5B2kwbzUAoefal4duvuF7asQ/xPWZvZr921K4fB1naATX5G+mqoE8RaHl1tX6MD52KBcBY25sejsJw9+m04Pp+ij7qtfGJAWlKemlpqHHKBcny/kCFKajKyEOvNwre9BxRLv0zNVcXAu41B0OClHb0hiHbBzkIpwJBXnnsXWeU0FLdV8A=";
             }
 
             @Override
-            public String world() {
+            public String getWorld() {
                 return "world";
             }
 
             @Override
-            public double x() {
+            public double getX() {
                 return -24.5;
             }
 
             @Override
-            public double y() {
+            public double getY() {
                 return 71;
             }
 
             @Override
-            public double z() {
+            public double getZ() {
                 return -58.5;
             }
 
             @Override
-            public float yaw() {
+            public float getYaw() {
                 return 0;
             }
 
             @Override
-            public float pitch() {
+            public float getPitch() {
                 return 0;
             }
 
             @Override
-            public boolean looking() {
+            public boolean isLooking() {
                 return true;
             }
 
